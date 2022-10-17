796428961d58678f153cdb1222f9ec75095fe9d8
sheikinstanislav <sheikinstanislav@gmail.com>
2022-10-17 16:46:45 +0300

    connect to db

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
+import dotenv from 'dotenv';
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
+dotenv.config();
 
-app.listen(5000, () => {
-  console.log('server start');
-});
+// Env Contants
+const PORT = process.env.PORT || 5001;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_NAME = process.env.DB_NAME;
+
+async function start() {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${DB_USER}:${DB_PASSWORD}@better-todo.spha4xt.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`
+    );
+
+    app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+start();
